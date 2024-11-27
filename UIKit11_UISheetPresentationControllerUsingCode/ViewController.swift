0b15d82b975bfc0b5f0589e28ffac407934cf25d
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-27 00:16:46 -0500

    The UISheetController was created

--- UIKit11_UISheetPresentationControllerUsingCode/ViewController.swift
+++ UIKit11_UISheetPresentationControllerUsingCode/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var swiftBetaButton: UIButton = {
+        var configuration = UIButton.Configuration.bordered()
+        configuration.title = "Suscribe to SwiftBeta!"
+        
+        let button = UIButton(type: .system, primaryAction: UIAction(handler: { _ in
+            self.presentSheetViewController()
+        }))
+        button.configuration = configuration
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .orange
+        view.addSubview(swiftBetaButton)
+        
+        NSLayoutConstraint.activate([
+            swiftBetaButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swiftBetaButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
+    func presentSheetViewController() {
+        let viewControllerToPresent = SheetViewController()
+        
+        if let sheet = viewControllerToPresent.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.selectedDetentIdentifier = .medium
+            sheet.prefersGrabberVisible = true
+            sheet.preferredCornerRadius = 20
+        }
+        
+        present(viewControllerToPresent, animated: true)
+    }
 
 }
 
